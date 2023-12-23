36d4477699ea932237eeeffefb857d91cd9b507d
hanishbansal231 <hanishbansal803@gmail.com>
2023-12-23 12:54:28 +0530

    Cloudinary and multer setup

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,34 @@
+import {v2 as cloudinary} from 'cloudinary';
+import fs from 'fs';
+
+
+// Cloudinary Setup
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+
+const uploadOnCloudinary = async (localPath) => {
+    try{
+        if(!localPath) return null;
+        
+        // upload file on cloudinary
+
+        const result = await cloudinary.uploader.upload(localPath,{
+            folder:'YouTube',
+            resource_type:'auto',
+        });
+
+        // console.log('File Upload Cloudinary -> ',result.url);
+
+        return result;
+
+    }catch(error){
+        fs.unlinkSync(localPath); // remove file
+        return null;
+    }
+}
+
+export default uploadOnCloudinary;
